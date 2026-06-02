12be4fe063f85c5325bdbd0248f5162b9f22a1f3
harebi <142986753+Fr20en@users.noreply.github.com>
2026-06-02 21:21:33 +0800

    Ultimate: Bypass WindowManager with addContentView and demote SurfaceView

--- app/src/main/java/com/android/support/XposedEntry.java
+++ app/src/main/java/com/android/support/XposedEntry.java
@@ -4,6 +4,8 @@ import android.app.Activity;
 import android.view.SurfaceView;
 import android.view.View;
 import android.view.ViewGroup;
+import android.view.WindowManager;
+import android.widget.FrameLayout;
 import android.widget.Toast;
 import de.robv.android.xposed.IXposedHookLoadPackage;
 import de.robv.android.xposed.XC_MethodHook;
@@ -13,6 +15,7 @@ import de.robv.android.xposed.callbacks.XC_LoadPackage;
 
 public class XposedEntry implements IXposedHookLoadPackage {
     private static boolean isHooked = false;
+    private static Activity currentActivity = null;
 
     @Override
     public void handleLoadPackage(XC_LoadPackage.LoadPackageParam lpparam) throws Throwable {
@@ -24,18 +27,18 @@ public class XposedEntry implements IXposedHookLoadPackage {
         XposedHelpers.findAndHookMethod(activityClass, "onResume", new XC_MethodHook() {
             @Override
             protected void afterHookedMethod(MethodHookParam param) throws Throwable {
+                currentActivity = (Activity) param.thisObject;
                 if (isHooked) return;
                 isHooked = true;
-                Activity activity = (Activity) param.thisObject;
                 XposedBridge.log("FGO Menu: UnityPlayerNativeActivity onResume!");
                 
-                activity.runOnUiThread(() -> {
+                currentActivity.runOnUiThread(() -> {
                     try {
-                        ViewGroup decor = (ViewGroup) activity.getWindow().getDecorView();
+                        ViewGroup decor = (ViewGroup) currentActivity.getWindow().getDecorView();
                         findAndDemoteSurfaceView(decor);
                         
-                        Toast.makeText(activity, "FGO Menu Injected!", Toast.LENGTH_SHORT).show();
-                        Menu menu = new Menu(activity);
+                        Toast.makeText(currentActivity, "FGO Menu Injected!", Toast.LENGTH_SHORT).show();
+                        Menu menu = new Menu(currentActivity);
                         menu.SetWindowManagerWindowService();
                         menu.ShowMenu();
                         XposedBridge.log("FGO Menu: Success!");
@@ -44,6 +47,62 @@ public class XposedEntry implements IXposedHookLoadPackage {
                         XposedBridge.log(t);
                     }
                 });
+            }
+        });
+
+        Class<?> wmImplClass = XposedHelpers.findClass("android.view.WindowManagerImpl", lpparam.classLoader);
+        
+        XposedHelpers.findAndHookMethod(wmImplClass, "addView", View.class, ViewGroup.LayoutParams.class, new XC_MethodHook() {
+            @Override
+            protected void beforeHookedMethod(MethodHookParam param) throws Throwable {
+                if (currentActivity == null) return;
+                View v = (View) param.args[0];
+                ViewGroup.LayoutParams lp = (ViewGroup.LayoutParams) param.args[1];
+                
+                FrameLayout.LayoutParams flp = new FrameLayout.LayoutParams(lp.width, lp.height);
+                if (lp instanceof WindowManager.LayoutParams) {
+                    WindowManager.LayoutParams wlp = (WindowManager.LayoutParams) lp;
+                    flp.leftMargin = wlp.x;
+                    flp.topMargin = wlp.y;
+                    flp.gravity = wlp.gravity;
+                }
+                
+                param.setResult(null);
+                currentActivity.addContentView(v, flp);
+                v.bringToFront();
+                XposedBridge.log("FGO Menu: Bypassed WindowManager! Used addContentView!");
+            }
+        });
+
+        XposedHelpers.findAndHookMethod(wmImplClass, "updateViewLayout", View.class, ViewGroup.LayoutParams.class, new XC_MethodHook() {
+            @Override
+            protected void beforeHookedMethod(MethodHookParam param) throws Throwable {
+                if (currentActivity == null) return;
+                View v = (View) param.args[0];
+                ViewGroup.LayoutParams lp = (ViewGroup.LayoutParams) param.args[1];
+                if (v.getParent() != null) {
+                    FrameLayout.LayoutParams flp = new FrameLayout.LayoutParams(lp.width, lp.height);
+                    if (lp instanceof WindowManager.LayoutParams) {
+                        WindowManager.LayoutParams wlp = (WindowManager.LayoutParams) lp;
+                        flp.leftMargin = wlp.x;
+                        flp.topMargin = wlp.y;
+                        flp.gravity = wlp.gravity;
+                    }
+                    param.setResult(null);
+                    v.setLayoutParams(flp);
+                    v.bringToFront();
+                }
+            }
+        });
+
+        XposedHelpers.findAndHookMethod(wmImplClass, "removeView", View.class, new XC_MethodHook() {
+            @Override
+            protected void beforeHookedMethod(MethodHookParam param) throws Throwable {
+                View v = (View) param.args[0];
+                if (v.getParent() instanceof ViewGroup) {
+                    param.setResult(null);
+                    ((ViewGroup) v.getParent()).removeView(v);
+                }
             }
         });
     }
