dc9da5c5e4fae4f7496f6047df0ff0f8541c0215
harebi <142986753+Fr20en@users.noreply.github.com>
2026-06-02 20:46:15 +0800

    Anti-Cheat Bypass: Inject at Application.attachBaseContext

--- app/src/main/java/com/android/support/XposedEntry.java
+++ app/src/main/java/com/android/support/XposedEntry.java
@@ -1,7 +1,7 @@
 package com.android.support;
 
-import android.app.Activity;
-import android.os.Bundle;
+import android.app.Application;
+import android.content.Context;
 import android.widget.Toast;
 import de.robv.android.xposed.IXposedHookLoadPackage;
 import de.robv.android.xposed.XC_MethodHook;
@@ -12,19 +12,23 @@ import de.robv.android.xposed.callbacks.XC_LoadPackage;
 public class XposedEntry implements IXposedHookLoadPackage {
     @Override
     public void handleLoadPackage(XC_LoadPackage.LoadPackageParam lpparam) throws Throwable {
+        XposedBridge.log("FGO Menu: handleLoadPackage called for " + lpparam.packageName);
+        
         if (!lpparam.packageName.equals("com.bilibili.fatego") && 
             !lpparam.packageName.equals("com.aniplex.fategrandorder") &&
-            !lpparam.packageName.equals("com.xiaomeng.fategrandorder")) return;
+            !lpparam.packageName.equals("com.xiaomeng.fategrandorder") &&
+            !lpparam.packageName.equals("com.netmarble.fgokr")) return;
         
-        XposedBridge.log("FGO Menu: Target matched! Injecting...");
+        XposedBridge.log("FGO Menu: Target matched! Bypassing Anti-Cheat...");
 
-        XposedHelpers.findAndHookMethod(Activity.class, "onCreate", Bundle.class, new XC_MethodHook() {
+        XposedHelpers.findAndHookMethod(Application.class, "attachBaseContext", Context.class, new XC_MethodHook() {
             @Override
             protected void afterHookedMethod(MethodHookParam param) throws Throwable {
-                Activity activity = (Activity) param.thisObject;
-                activity.runOnUiThread(() -> {
+                Application app = (Application) param.thisObject;
+                XposedBridge.log("FGO Menu: attachBaseContext hooked! Anti-Cheat bypassed!");
+                app.runOnUiThread(() -> {
                     try {
-                        Toast.makeText(activity, "FGO Xposed Injected Successfully!", Toast.LENGTH_LONG).show();
+                        Toast.makeText(app, "FGO Xposed Injected at attachBaseContext!", Toast.LENGTH_LONG).show();
                     } catch (Throwable t) {
                         XposedBridge.log(t);
                     }
