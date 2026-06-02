b04dca53b06a7eca27af861b91adb2a0e2ea3620
harebi <142986753+Fr20en@users.noreply.github.com>
2026-06-02 20:32:33 +0800

    Test: Minimal injection verification

--- app/src/main/java/com/android/support/XposedEntry.java
+++ app/src/main/java/com/android/support/XposedEntry.java
@@ -1,7 +1,6 @@
 package com.android.support;
 
 import android.app.Activity;
-import android.app.Application;
 import android.os.Bundle;
 import android.widget.Toast;
 import de.robv.android.xposed.IXposedHookLoadPackage;
@@ -13,27 +12,16 @@ import de.robv.android.xposed.callbacks.XC_LoadPackage;
 public class XposedEntry implements IXposedHookLoadPackage {
     @Override
     public void handleLoadPackage(XC_LoadPackage.LoadPackageParam lpparam) throws Throwable {
-        XposedBridge.log("FGO Menu: handleLoadPackage called for " + lpparam.packageName);
-        
         if (!lpparam.packageName.equals("com.bilibili.fatego") && 
             !lpparam.packageName.equals("com.aniplex.fategrandorder") &&
-            !lpparam.packageName.equals("com.xiaomeng.fategrandorder") &&
-            !lpparam.packageName.equals("com.netease.fgo")) return;
+            !lpparam.packageName.equals("com.xiaomeng.fategrandorder")) return;
         
         XposedBridge.log("FGO Menu: Target matched! Injecting...");
-
-        XposedHelpers.findAndHookMethod(Application.class, "onCreate", new XC_MethodHook() {
-            @Override
-            protected void afterHookedMethod(MethodHookParam param) throws Throwable {
-                XposedBridge.log("FGO Menu: Application.onCreate hooked!");
-            }
-        });
 
         XposedHelpers.findAndHookMethod(Activity.class, "onCreate", Bundle.class, new XC_MethodHook() {
             @Override
             protected void afterHookedMethod(MethodHookParam param) throws Throwable {
                 Activity activity = (Activity) param.thisObject;
-                XposedBridge.log("FGO Menu: Activity.onCreate hooked: " + activity.getClass().getName());
                 activity.runOnUiThread(() -> {
                     try {
                         Toast.makeText(activity, "FGO Xposed Injected Successfully!", Toast.LENGTH_LONG).show();
