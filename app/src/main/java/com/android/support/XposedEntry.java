e39c1c423d7654b0639b1c518048c63ba723bc06
harebi <142986753+Fr20en@users.noreply.github.com>
2026-06-02 19:44:16 +0800

    Add XposedEntry

---
+++ app/src/main/java/com/android/support/XposedEntry.java
@@ -0,0 +1,21 @@
+package com.android.support;
+import android.app.Application;
+import android.content.Context;
+import de.robv.android.xposed.IXposedHookLoadPackage;
+import de.robv.android.xposed.XC_MethodHook;
+import de.robv.android.xposed.XposedHelpers;
+import de.robv.android.xposed.callbacks.XC_LoadPackage;
+
+public class XposedEntry implements IXposedHookLoadPackage {
+    @Override
+    public void handleLoadPackage(XC_LoadPackage.LoadPackageParam lpparam) throws Throwable {
+        if (!lpparam.packageName.equals("com.bilibili.fatego")) return;
+        XposedHelpers.findAndHookMethod(Application.class, "onCreate", new XC_MethodHook() {
+            @Override
+            protected void afterHookedMethod(MethodHookParam param) throws Throwable {
+                Context ctx = ((Application) param.thisObject).getApplicationContext();
+                Menu.Start(ctx);
+            }
+        });
+    }
+}
