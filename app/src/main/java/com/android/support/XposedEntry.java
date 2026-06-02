d36e1431fa5a114fe2be540c79ea3dc29b5286c5
harebi <142986753+Fr20en@users.noreply.github.com>
2026-06-02 21:37:59 +0800

    Ultimate: Start MenuActivity in external process to bypass Unity and Anti-Cheat

--- app/src/main/java/com/android/support/XposedEntry.java
+++ app/src/main/java/com/android/support/XposedEntry.java
@@ -1,11 +1,8 @@
 package com.android.support;
 
 import android.app.Activity;
-import android.graphics.Color;
-import android.view.Gravity;
-import android.view.ViewGroup;
-import android.widget.FrameLayout;
-import android.widget.TextView;
+import android.content.Intent;
+import android.os.Bundle;
 import de.robv.android.xposed.IXposedHookLoadPackage;
 import de.robv.android.xposed.XC_MethodHook;
 import de.robv.android.xposed.XposedHelpers;
@@ -24,33 +21,19 @@ public class XposedEntry implements IXposedHookLoadPackage {
         XposedHelpers.findAndHookMethod(Activity.class, "onResume", new XC_MethodHook() {
             @Override
             protected void afterHookedMethod(MethodHookParam param) throws Throwable {
-                Activity activity = (Activity) param.thisObject;
-                String cls = activity.getClass().getName();
-                XposedBridge.log("FGO Menu: Activity onResume: " + cls);
-                
                 if (isHooked) return;
                 isHooked = true;
+                Activity activity = (Activity) param.thisObject;
                 
                 activity.runOnUiThread(() -> {
                     try {
-                        ViewGroup decor = (ViewGroup) activity.getWindow().getDecorView();
-                        TextView tv = new TextView(activity);
-                        tv.setText("晴酱的终极测试！如果你能看到这行字，说明注入成功！");
-                        tv.setTextSize(30);
-                        tv.setBackgroundColor(Color.RED);
-                        tv.setTextColor(Color.WHITE);
-                        tv.setGravity(Gravity.CENTER);
-                        
-                        FrameLayout.LayoutParams flp = new FrameLayout.LayoutParams(
-                            FrameLayout.LayoutParams.MATCH_PARENT,
-                            FrameLayout.LayoutParams.MATCH_PARENT
-                        );
-                        decor.addView(tv, flp);
-                        tv.bringToFront();
-                        XposedBridge.log("FGO Menu: DecorView addView SUCCESS!");
+                        XposedBridge.log("FGO Menu: Starting external MenuActivity!");
+                        Intent intent = new Intent();
+                        intent.setClassName("com.android.support", "com.android.support.MenuActivity");
+                        intent.addFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
+                        activity.startActivity(intent);
                     } catch (Throwable t) {
                         XposedBridge.log("FGO Menu FATAL: " + t.getMessage());
-                        XposedBridge.log(t);
                     }
                 });
             }
