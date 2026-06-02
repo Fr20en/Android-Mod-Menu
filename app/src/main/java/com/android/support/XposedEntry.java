1f14ec9be8c6d5df41a80024c97ae9148bf1a827
harebi <142986753+Fr20en@users.noreply.github.com>
2026-06-02 21:36:48 +0800

    Ultimate: Hook onWindowFocusChanged and call SimpleMenu directly

--- app/src/main/java/com/android/support/XposedEntry.java
+++ app/src/main/java/com/android/support/XposedEntry.java
@@ -1,7 +1,7 @@
 package com.android.support;
 
-import android.app.Application;
-import android.content.Context;
+import android.app.Activity;
+import android.os.Bundle;
 import de.robv.android.xposed.IXposedHookLoadPackage;
 import de.robv.android.xposed.XC_MethodHook;
 import de.robv.android.xposed.XposedHelpers;
@@ -9,18 +9,29 @@ import de.robv.android.xposed.XposedBridge;
 import de.robv.android.xposed.callbacks.XC_LoadPackage;
 
 public class XposedEntry implements IXposedHookLoadPackage {
+    private static boolean isHooked = false;
+
     @Override
     public void handleLoadPackage(XC_LoadPackage.LoadPackageParam lpparam) throws Throwable {
-        XposedBridge.log("FGO Menu: handleLoadPackage called for " + lpparam.packageName);
-        
         if (!lpparam.packageName.equals("com.bilibili.fatego")) return;
         
-        XposedBridge.log("FGO Menu: Bili FGO matched! Injecting...");
+        XposedBridge.log("FGO Menu: Target matched! " + lpparam.packageName);
 
-        XposedHelpers.findAndHookMethod(Application.class, "attachBaseContext", Context.class, new XC_MethodHook() {
+        Class<?> targetActivity = XposedHelpers.findClass("com.bilibili.fatego.UnityPlayerNativeActivity", lpparam.classLoader);
+        
+        XposedHelpers.findAndHookMethod(targetActivity, "onWindowFocusChanged", boolean.class, new XC_MethodHook() {
             @Override
             protected void afterHookedMethod(MethodHookParam param) throws Throwable {
-                XposedBridge.log("FGO Menu: attachBaseContext SUCCESS! Code is running!");
+                boolean hasFocus = (boolean) param.args[0];
+                if (!hasFocus || isHooked) return;
+                isHooked = true;
+                
+                Activity activity = (Activity) param.thisObject;
+                XposedBridge.log("FGO Menu: UnityPlayerNativeActivity got focus!");
+                
+                activity.runOnUiThread(() -> {
+                    SimpleMenu.show(activity);
+                });
             }
         });
     }
