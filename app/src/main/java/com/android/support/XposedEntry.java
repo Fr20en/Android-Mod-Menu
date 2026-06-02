af5a8228e233d39a13075b8dee035aa36ed663b0
harebi <142986753+Fr20en@users.noreply.github.com>
2026-06-02 21:23:33 +0800

    Ultimate: Bypass WindowManager with addContentView and demote SurfaceView

--- app/src/main/java/com/android/support/XposedEntry.java
+++ app/src/main/java/com/android/support/XposedEntry.java
@@ -1,6 +1,7 @@
 package com.android.support;
 
 import android.app.Activity;
+import android.os.Bundle;
 import android.view.SurfaceView;
 import android.view.View;
 import android.view.ViewGroup;
@@ -19,37 +20,27 @@ public class XposedEntry implements IXposedHookLoadPackage {
 
     @Override
     public void handleLoadPackage(XC_LoadPackage.LoadPackageParam lpparam) throws Throwable {
-        if (!lpparam.packageName.equals("com.bilibili.fatego")) return;
-        XposedBridge.log("FGO Menu: Bili FGO detected!");
-
-        Class<?> activityClass = XposedHelpers.findClass("com.bilibili.fatego.UnityPlayerNativeActivity", lpparam.classLoader);
+        if (!lpparam.packageName.equals("com.bilibili.fatego") && 
+            !lpparam.packageName.equals("com.aniplex.fategrandorder") &&
+            !lpparam.packageName.equals("com.xiaomeng.fategrandorder")) return;
         
-        XposedHelpers.findAndHookMethod(activityClass, "onResume", new XC_MethodHook() {
+        XposedBridge.log("FGO Menu: Target matched! " + lpparam.packageName);
+
+        // 1. 废掉 SurfaceView 的置顶能力
+        XposedHelpers.findAndHookMethod(SurfaceView.class, "setZOrderOnTop", boolean.class, new XC_MethodHook() {
             @Override
-            protected void afterHookedMethod(MethodHookParam param) throws Throwable {
-                currentActivity = (Activity) param.thisObject;
-                if (isHooked) return;
-                isHooked = true;
-                XposedBridge.log("FGO Menu: UnityPlayerNativeActivity onResume!");
-                
-                currentActivity.runOnUiThread(() -> {
-                    try {
-                        ViewGroup decor = (ViewGroup) currentActivity.getWindow().getDecorView();
-                        findAndDemoteSurfaceView(decor);
-                        
-                        Toast.makeText(currentActivity, "FGO Menu Injected!", Toast.LENGTH_SHORT).show();
-                        Menu menu = new Menu(currentActivity);
-                        menu.SetWindowManagerWindowService();
-                        menu.ShowMenu();
-                        XposedBridge.log("FGO Menu: Success!");
-                    } catch (Throwable t) {
-                        XposedBridge.log("FGO Menu Error: " + t.getMessage());
-                        XposedBridge.log(t);
-                    }
-                });
+            protected void beforeHookedMethod(MethodHookParam param) throws Throwable {
+                param.args[0] = false;
+            }
+        });
+        XposedHelpers.findAndHookMethod(SurfaceView.class, "setZOrderMediaOverlay", boolean.class, new XC_MethodHook() {
+            @Override
+            protected void beforeHookedMethod(MethodHookParam param) throws Throwable {
+                param.args[0] = false;
             }
         });
 
+        // 2. 代理 WindowManager，把悬浮窗强行塞进 DecorView
         Class<?> wmImplClass = XposedHelpers.findClass("android.view.WindowManagerImpl", lpparam.classLoader);
         
         XposedHelpers.findAndHookMethod(wmImplClass, "addView", View.class, ViewGroup.LayoutParams.class, new XC_MethodHook() {
@@ -105,17 +96,48 @@ public class XposedEntry implements IXposedHookLoadPackage {
                 }
             }
         });
+
+        // 3. 精准 Hook B服主 Activity
+        try {
+            Class<?> activityClass = XposedHelpers.findClass("com.bilibili.fatego.UnityPlayerNativeActivity", lpparam.classLoader);
+            XposedHelpers.findAndHookMethod(activityClass, "onResume", new XC_MethodHook() {
+                @Override
+                protected void afterHookedMethod(MethodHookParam param) throws Throwable {
+                    currentActivity = (Activity) param.thisObject;
+                    if (isHooked) return;
+                    isHooked = true;
+                    XposedBridge.log("FGO Menu: UnityPlayerNativeActivity onResume!");
+                    
+                    currentActivity.runOnUiThread(() -> {
+                        try {
+                            ViewGroup decor = (ViewGroup) currentActivity.getWindow().getDecorView();
+                            demoteSurfaceViews(decor);
+                            
+                            Toast.makeText(currentActivity, "FGO Menu Injected!", Toast.LENGTH_SHORT).show();
+                            Menu menu = new Menu(currentActivity);
+                            menu.SetWindowManagerWindowService();
+                            menu.ShowMenu();
+                            XposedBridge.log("FGO Menu: Success!");
+                        } catch (Throwable t) {
+                            XposedBridge.log("FGO Menu Error: " + t.getMessage());
+                            XposedBridge.log(t);
+                        }
+                    });
+                }
+            });
+        } catch (Throwable t) {
+            XposedBridge.log("FGO Menu: Failed to hook UnityPlayerNativeActivity: " + t.getMessage());
+        }
     }
     
-    private void findAndDemoteSurfaceView(ViewGroup group) {
+    private void demoteSurfaceViews(ViewGroup group) {
         for (int i = 0; i < group.getChildCount(); i++) {
             View child = group.getChildAt(i);
             if (child instanceof SurfaceView) {
                 ((SurfaceView) child).setZOrderOnTop(false);
                 ((SurfaceView) child).setZOrderMediaOverlay(false);
-                XposedBridge.log("FGO Menu: Demoted SurfaceView Z-Order!");
             } else if (child instanceof ViewGroup) {
-                findAndDemoteSurfaceView((ViewGroup) child);
+                demoteSurfaceViews((ViewGroup) child);
             }
         }
     }
