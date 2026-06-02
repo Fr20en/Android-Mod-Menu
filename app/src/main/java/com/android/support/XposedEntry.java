cb8313014cf5d1fbc7bad942449ae3707a628bb1
harebi <142986753+Fr20en@users.noreply.github.com>
2026-06-02 20:16:53 +0800

    Fix: Hook Activity.onCreate to get Activity Context for floating window

--- app/src/main/java/com/android/support/XposedEntry.java
+++ app/src/main/java/com/android/support/XposedEntry.java
@@ -1,20 +1,39 @@
 package com.android.support;
-import android.app.Application;
-import android.content.Context;
+
+import android.app.Activity;
+import android.os.Bundle;
 import de.robv.android.xposed.IXposedHookLoadPackage;
 import de.robv.android.xposed.XC_MethodHook;
 import de.robv.android.xposed.XposedHelpers;
+import de.robv.android.xposed.XposedBridge;
 import de.robv.android.xposed.callbacks.XC_LoadPackage;
 
 public class XposedEntry implements IXposedHookLoadPackage {
+    private static boolean isHooked = false;
+
     @Override
     public void handleLoadPackage(XC_LoadPackage.LoadPackageParam lpparam) throws Throwable {
-        if (!lpparam.packageName.equals("com.bilibili.fatego")) return;
-        XposedHelpers.findAndHookMethod(Application.class, "onCreate", new XC_MethodHook() {
+        if (!lpparam.packageName.equals("com.bilibili.fatego") && 
+            !lpparam.packageName.equals("com.aniplex.fategrandorder") && 
+            !lpparam.packageName.equals("com.xiaomeng.fategrandorder")) return;
+        
+        XposedBridge.log("FGO Mod Menu: Hooked " + lpparam.packageName);
+
+        XposedHelpers.findAndHookMethod(Activity.class, "onCreate", Bundle.class, new XC_MethodHook() {
             @Override
             protected void afterHookedMethod(MethodHookParam param) throws Throwable {
-                Context ctx = ((Application) param.thisObject).getApplicationContext();
-                new Menu(ctx);
+                if (!isHooked) {
+                    isHooked = true;
+                    Activity activity = (Activity) param.thisObject;
+                    activity.runOnUiThread(() -> {
+                        try {
+                            XposedBridge.log("FGO Mod Menu: Starting Menu UI");
+                            new Menu(activity);
+                        } catch (Exception e) {
+                            XposedBridge.log(e);
+                        }
+                    });
+                }
             }
         });
     }
